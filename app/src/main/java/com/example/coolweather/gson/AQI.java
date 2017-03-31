362a6c673b80a8b87a63e1934d826e595d744a60
Dixon <547545357@qq.com>
2017-03-31 15:42:21 +0800

    新增切换城市和手动更新的功能。

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by Administrator on 2017/3/28 0028.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity {
+        public String aqi;
+        public String pm25;
+    }
+}
--- app/src/main/java/com/example/coolweather/util/Utility.java
+++ app/src/main/java/com/example/coolweather/util/Utility.java
@@ -5,6 +5,9 @@ import android.text.TextUtils;
 import com.example.coolweather.db.City;
 import com.example.coolweather.db.County;
 import com.example.coolweather.db.Province;
+import com.example.coolweather.gson.Weather;
+import com.google.gson.Gson;
+import com.google.gson.internal.Excluder;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -79,5 +82,19 @@ public class Utility {
             }
         }
         return false;
+    }
+    /**
+     * 将返回的 JSON 数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
